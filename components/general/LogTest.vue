e0beab387353339b13a5a200eb58c72a70d72568
Ayumi <ayumi.harun@gmail.com>
2022-05-13 19:45:31 -0500

    last commit, adding taxesto incomes

--- components/general/LogTest.vue
+++ components/general/LogTest.vue
@@ -10,14 +10,14 @@ export default {
         hasSesion() {
             if( !localStorage.getItem('user') ){
                 alert( 'No se encontró una sesión activa!, se te redireccionará al Inicio');
-                window.location.href = "/";
+                this.$router.push('/');      
             }
         },
         // test if user has required level
         hasLevel( reqLvl ) {
             if( localStorage.getItem('level') > reqLvl ){
                 alert( 'No tienes permiso de entrar a esta sección, se te redireccionará al Inicio');
-                window.location.href = "/";
+                this.$router.push('/');      
             }
         }
     }
